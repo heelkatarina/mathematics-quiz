d5c65ee61b6fca721bb4be9c23a029262f8ff08c
heelkatarina <heelkatarina@gmail.com>
2025-03-21 02:55:05 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,7 @@
+<?php
+// Sample PHP code for a mathematical problem
+function calculateSquareRoot($number) {
+    $squareRoot = sqrt($number);
+    echo "The square root of $number is: $squareRoot";
+}
+?>
