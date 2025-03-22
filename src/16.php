f5d2454ac7c1bb7e21d8b64a5765ed4fe5947598
heelkatarina <heelkatarina@gmail.com>
2025-03-22 16:10:38 +0000

    Add 16.php

---
+++ src/16.php
@@ -0,0 +1,12 @@
+<?php
+// This PHP code is designed to be run in a web environment.
+// Here's a simple example of how you might use it:
+
+// Define variables
+$name = "Alice";
+$age = 18;
+$schoolYear = 2023;
+
+// Output the data as text
+echo "Hello, $name! You are $age years old and in your $schoolYear school year.";
+?>
