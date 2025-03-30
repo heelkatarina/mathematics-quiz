f928692e9357f730578fe47476ff406d6174f0dc
heelkatarina <heelkatarina@gmail.com>
2025-03-30 20:27:08 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,13 @@
+<?php
+// Define variables and constants
+$vars = array(
+    "x" => 5,
+    "y" => 10,
+    "z" => 20
+);
+
+// Calculate a random value using math operations
+$result = ($vars["x"] + $vars["y"]) * $vars["z"];
+
+echo "The result is: $result";
+?>
