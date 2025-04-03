52c2fccde58fde517d39f2b9cb8fa74855c17b7f
heelkatarina <heelkatarina@gmail.com>
2025-04-03 23:08:11 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,24 @@
+<?php
+// Sample PHP code to demonstrate mathematical operations
+
+// Addition
+$sum = 10 + 5;
+echo "The sum of 10 and 5 is: $sum";
+
+// Subtraction
+$result = 10 - 3;
+echo "The result of 10 minus 3 is: $result";
+
+// Multiplication
+$product = 7 * 4;
+echo "The product of 7 and 4 is: $product";
+
+// Division (integer division)
+$quotient = 15 / 3;
+echo "The quotient of 15 divided by 3 is: $quotient";
+
+// Modulus (remainder after division)
+$factorial = 6 * 5 * 4 * 3 * 2 * 1;
+echo "The factorial of 6 is: $factorial";
+
+?>
