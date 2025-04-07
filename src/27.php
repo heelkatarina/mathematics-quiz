b040fa08fbe5686567de9889a71cc2211e7a1e43
heelkatarina <heelkatarina@gmail.com>
2025-04-07 16:58:00 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,18 @@
+<?php
+// This is a simple PHP script to demonstrate basic mathematical operations.
+
+// Addition of two numbers
+echo "2 + 3 = " . (2 + 3) . "<br>";
+
+// Subtraction of two numbers
+echo "5 - 2 = " . (5 - 2) . "<br>";
+
+// Multiplication of two numbers
+echo "4 * 6 = " . (4 * 6) . "<br>";
+
+// Division of two numbers (integer division)
+echo "8 / 3 = " . (8 / 3) . "<br>";
+
+// Floor division (rounding down to the nearest whole number)
+echo "10 % 2 = " . (10 % 2) . "<br>";
+?>
