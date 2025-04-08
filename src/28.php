73973a20284553e7cff5d567b868201fe672c8c0
heelkatarina <heelkatarina@gmail.com>
2025-04-08 05:15:34 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables
+$var1 = "Value 1";
+$var2 = "Value 2";
+
+// Perform some calculation
+$result = $var1 + $var2;
+
+// Output result to user or display in console
+echo "The sum of the two values is: $result";
+?>
