d025fb401edd3152dc90d93d00444bd1cccdde16
heelkatarina <heelkatarina@gmail.com>
2025-04-24 15:08:21 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,22 @@
+<?php
+// Example PHP code for basic arithmetic operations
+
+$first_number = 5;
+$second_number = 3;
+
+// Addition
+$result_addition = $first_number + $second_number;
+echo "The sum of $first_number and $second_number is: $result_addition";
+
+// Subtraction
+$result_subtraction = $first_number - $second_number;
+echo "The difference between $first_number and $second_number is: $result_subtraction";
+
+// Multiplication
+$result_multiplication = $first_number * $second_number;
+echo "The product of $first_number and $second_number is: $result_multiplication";
+
+// Division
+$result_division = $first_number / $second_number;
+echo "The quotient of $first_number divided by $second_number is: $result_division";
+?>
