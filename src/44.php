be2e3c9d3f5245cc553210ba2216aaed77e28b25
heelkatarina <heelkatarina@gmail.com>
2025-05-25 12:04:50 +0000

    Add 44.php

---
+++ src/44.php
@@ -0,0 +1,11 @@
+<?php
+// Define some variables
+$x = 5;
+$y = 10;
+
+// Perform arithmetic operations
+$result = $x + $y * 2 - $x / 3 + $y * 4;
+
+// Output the result
+echo "The result of the arithmetic operation is: $result";
+?>
